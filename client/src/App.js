42c60d84e62eeb3b48729069201ea3a9cbfdb8e9
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-28 10:35:56 +0530

    stylesheets

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import {Button } from 'antd';
-
 import React from 'react'
+import './stylesheets/alignments.css'
+import './stylesheets/custom-components.css'
+import './stylesheets/form-elements.css'
+import './stylesheets/text-elements.css'
+import './stylesheets/theme.css'
+
 
 const App = () => {
   return (
-    // <div>App</div>
-    <Button type="primary">Primary</Button>
-
+    <>
+    <div className='bg-primary h-screen flex items-center justify-center'>
+      <h1>Banergy e-Wallet</h1>
+      <Button type="primary">Primary</Button>
+    </div>
+    </>
     )
 }
 
